c500db37ecd017eacf2161e7ecf4be5779b5e688
Dominik Hellweg <dominik.hellweg@cdi-ag.de>
2023-09-12 21:55:12 +0200

    fix

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,9 @@
 import * as festivalController from '$lib/services/festival-event-service';
 import type { PageServerLoad } from '../../.svelte-kit/types/src/routes/$types';
+import type { BackendFestivalListItem } from '$lib/models/BackendFestivalListItem';
+import type { FrontendFestivalEvent } from '$lib/models/FrontendFestivalEvent';
 
 export const load = (async () => {
-	return await festivalController.getAllFestivals();
+	const festivalEvents: FrontendFestivalEvent[] = await festivalController.getAllFestivals();
+	return {festivalEvents: festivalEvents};
 }) satisfies PageServerLoad;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
 	import type { FrontendFestivalEvent } from '$lib/models/FrontendFestivalEvent';
 
-	export let data: FrontendFestivalEvent[];
+	export let data: { festivalEvents: FrontendFestivalEvent[] };
 </script>
 
 <h1>Festivals</h1>
 
 <div>
 	<p>List:</p>
-	{#each data as loadedEvent}
+	{#each data.festivalEvents as loadedEvent}
 		<ul>
 			<li>
 				<a href="/festival/{loadedEvent.id}">{loadedEvent.name}</a>
