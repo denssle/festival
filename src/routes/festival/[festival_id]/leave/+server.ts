b352598e72c419b14c86eb5e8a065d5e5b02efe3
Dominik Hellweg <dominik.hellweg@cdi-ag.de>
2023-10-24 18:47:37 +0200

    Update +server.ts

--- src/routes/festival/[festival_id]/leave/+server.ts
+++ src/routes/festival/[festival_id]/leave/+server.ts
@@ -1,7 +1,7 @@
 import { leaveFestival } from '$lib/services/festival-event-service';
 import { extractUser } from '$lib/services/user-service';
 
-export async function POST({ params, cookies }) {
+export async function POST({ params, cookies }): Promise<Response> {
 	await leaveFestival(extractUser(cookies.get('session')), params.festival_id);
 	return new Response(null, { status: 200 });
 }
