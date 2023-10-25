4d1ec32a9895ce778584c3dcf6947712adc940fb
Dominik Hellweg <denssle@protonmail.com>
2023-10-25 20:58:46 +0200

    add ignore

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,5 +1,6 @@
 import type { PageServerLoad } from '../../.svelte-kit/types/src/routes/$types';
 
+// @ts-ignore
 export const load = (({ locals }) => {
 	return locals;
 }) satisfies PageServerLoad;
--- src/routes/festival/[festival_id]/+server.ts
+++ src/routes/festival/[festival_id]/+server.ts
@@ -1,6 +1,7 @@
 import { extractUser } from '$lib/services/user-service';
 import { deleteFestival } from '$lib/services/festival-event-service';
 
+// @ts-ignore
 export function DELETE({ params, request, cookies }) {
 	deleteFestival(extractUser(cookies.get('session')), params.festival_id);
 	return new Response(null, { status: 303 });
