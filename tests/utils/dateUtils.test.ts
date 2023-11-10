0e71c29854bdc7cabc333da856fc8b05ba9721b4
Dominik Hellweg <dominik.hellweg@cdi-ag.de>
2023-11-10 14:15:13 +0100

    fixed this fucking tests

--- tests/utils/dateUtils.test.ts
+++
@@ -1,6 +0,0 @@
-import {describe, expect, test} from '@jest/globals';
-import { createDateFromStrings } from '$lib/utils/dateUtils';
-
-test('test createDateFromStrings', () => {
-	expect(createDateFromStrings('2023-11-05', '16:00')).toBe(null);
-});
