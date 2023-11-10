e6a39f8a21bc93e59cb89e146c75f3b18c418ade
Dominik Hellweg <dominik.hellweg@cdi-ag.de>
2023-11-10 14:15:24 +0100

    fixed this fucking tests 2

---
+++ src/tests/utils/dateUtils.test.ts
@@ -0,0 +1,6 @@
+import { expect, test } from '@jest/globals';
+import { createDateFromStrings } from '../../lib/utils/dateUtils';
+
+test('test createDateFromStrings', () => {
+	expect(createDateFromStrings('2023-11-05', '16:00')).toBe(null);
+});
