3a4165ef8d6c91812168ad2ae50cfc5f7a0342f2
Dominik Hellweg <dominik.hellweg@cdi-ag.de>
2023-11-30 16:42:48 +0100

    remove unused export

--- src/routes/festival/[festival_id]/+server.ts
+++ src/routes/festival/[festival_id]/+server.ts
@@ -3,7 +3,7 @@ import { extractUser } from '$lib/services/user-service';
 import type { RequestHandler } from '@sveltejs/kit';
 
 // TODO Add Types to the parameters
-export const DELETE: RequestHandler = async ({ cookies, params } ): Promise<Response> => {
+export const DELETE: RequestHandler = async ({ cookies, params }): Promise<Response> => {
 	if (params && params.festival_id) {
 		await deleteFestival(extractUser(cookies.get('session')), params.festival_id);
 		return new Response(null, { status: 303 });
