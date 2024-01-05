ac8deae2798c617d14af3afd756aa7f06572c220
Dominik Hellweg <dominik.hellweg@cdi-ag.de>
2024-01-05 08:21:32 +0100

    refactor

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,11 +1,11 @@
 import type { Actions, Cookies } from '@sveltejs/kit';
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from '../../../.svelte-kit/types/src/routes/$types';
+import * as userService from '$lib/services/user-service';
 import { login, validateSessionToken } from '$lib/services/user-service';
 import type { BackendUser } from '$lib/models/BackendUser';
-import type { StandardResponse } from '$lib/models/StandardResponse';
 import type { UserFormData } from '$lib/models/UserFormData';
-import * as userService from '$lib/services/user-service';
+import { StandardResponse } from '$lib/models/StandardResponse';
 
 export const load: PageServerLoad = async ({ cookies }: { cookies: Cookies }): Promise<StandardResponse> => {
 	const valid: boolean = await validateSessionToken(cookies.get('session'));
@@ -13,13 +13,11 @@ export const load: PageServerLoad = async ({ cookies }: { cookies: Cookies }): P
 	if (valid) {
 		redirect(303, '/');
 	}
-	return {
-		success: true
-	};
+	return { success: true };
 };
 
 export const actions: Actions = {
-	default: async ({ cookies, request }): Promise<StandardResponse> => {
+	default: async ({ cookies, request }: { cookies: Cookies, request: Request }): Promise<StandardResponse> => {
 		const formData: UserFormData = await userService.readFormDataFrontEndUser(request.formData());
 		if (formData.nickname && formData.password) {
 			const user: BackendUser | null = await login(formData.nickname, formData.password);
