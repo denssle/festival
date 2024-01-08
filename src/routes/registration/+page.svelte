45c29668fce40337891ce978f425023242e0caa5
Dominik Hellweg <dominik.hellweg@cdi-ag.de>
2024-01-08 14:06:02 +0100

    idea

--- src/routes/registration/+page.svelte
+++ src/routes/registration/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 	import type { NicknamePasswordFormData } from '$lib/models/NicknamePasswordFormData';
 	import NicknamePasswordForm from '$lib/sharedComponents/NicknamePasswordForm.svelte';
+	import type { ActionData } from '../../../.svelte-kit/types/src/routes/registration/$types';
 
 	export let data: NicknamePasswordFormData;
+	export let form: ActionData;
+	console.log(form);
 </script>
 
 <NicknamePasswordForm {data} headline={'Registration'}>
