64f3dcf49d736c3cf5734d67fbebe0a9c7bd41a2
Dominik Hellweg <dominik.hellweg@cdi-ag.de>
2024-02-01 20:43:09 +0100

    format

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,4 +1,4 @@
-<script lang='ts'>
+<script lang="ts">
 	import uberspaceBanner from '../../images/uberspace_badge_dark.png';
 
 	export let data: { version: string };
@@ -7,16 +7,18 @@
 <article>
 	<h2>Über diese Seite</h2>
 	<section>
-		<p>Diese Seite wurde erstellt mit <a href='https://kit.svelte.dev/'>svelte</a>.
-			Das Styling kommt von <a href='https://simplecss.org/'>simple css </a> und die Daten werden gespeichert in einer
-			<a href='https://redis.io/'>redis</a>
-			Datenbank auf <a href='https://upstash.com/'>upstash</a>.
+		<p>
+			Diese Seite wurde erstellt mit <a href="https://kit.svelte.dev/">svelte</a>. Das Styling kommt von
+			<a href="https://simplecss.org/">simple css </a>
+			und die Daten werden gespeichert in einer
+			<a href="https://redis.io/">redis</a>
+			Datenbank auf <a href="https://upstash.com/">upstash</a>.
 		</p>
 	</section>
 	<section>
-		<img src={uberspaceBanner} alt='uberspace Banner' class='center' />
-		<p style='text-align: center'>
-			Die Seite ist gehostet auf <a href='https://uberspace.de/'>uberspace</a>.
+		<img src={uberspaceBanner} alt="uberspace Banner" class="center" />
+		<p style="text-align: center">
+			Die Seite ist gehostet auf <a href="https://uberspace.de/">uberspace</a>.
 		</p>
 	</section>
 	<section>
@@ -25,9 +27,9 @@
 </article>
 
 <style>
-    .center {
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
-    }
+	.center {
+		display: block;
+		margin-left: auto;
+		margin-right: auto;
+	}
 </style>
