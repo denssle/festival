d54f0d27e1a66c3d7425952abf712992ee14972e
Dominik Hellweg <dominik.hellweg@cdi-ag.de>
2024-02-22 15:50:59 +0100

    set currentuser on every request

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -7,17 +7,26 @@ const noAuthURLs: string[] = ['/login', '/registration', '/about', '/impressum']
 
 export const handle: Handle = async ({ event, resolve }): Promise<Response> => {
 	const pathname: string = event.url.pathname;
+	const sessionCookie: string | undefined = event.cookies.get('session');
+	const valid: boolean = await userController.validateSessionToken(sessionCookie);
+	const currentUser: BackendUser | null = userController.extractUser(sessionCookie);
+	if (currentUser) {
+		event.locals.currentUser = {
+			isAuthenticated: valid,
+			id: currentUser.id,
+			email: currentUser.email,
+			nickname: currentUser.nickname
+		};
+	} else {
+		event.locals.currentUser = {
+			isAuthenticated: false,
+			id: '',
+			email: '',
+			nickname: ''
+		};
+	}
 	if (!noAuthURLs.includes(pathname)) {
-		const sessionCookie: string | undefined = event.cookies.get('session');
-		const valid: boolean = await userController.validateSessionToken(sessionCookie);
-		const currentUser: BackendUser | null = userController.extractUser(sessionCookie);
 		if (valid && currentUser) {
-			event.locals.currentUser = {
-				isAuthenticated: valid,
-				id: currentUser.id,
-				email: currentUser.email,
-				nickname: currentUser.nickname
-			};
 			return resolve(event);
 		} else {
 			return new Response('Redirect', { status: 303, headers: { Location: '/login' } });
