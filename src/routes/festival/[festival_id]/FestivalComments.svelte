18d0a11fac59d8ba65fb4b13af8df73136c799c1
Dominik Hellweg <dominik.hellweg@cdi-ag.de>
2024-05-09 22:37:44 +0200

    connect endpoint

--- src/routes/festival/[festival_id]/FestivalComments.svelte
+++ src/routes/festival/[festival_id]/FestivalComments.svelte
@@ -1,30 +1,22 @@
-<script lang="ts">
+<script lang='ts'>
+
 	export let festivalId: string = '';
 
-	async function handleSubmit(event: { currentTarget: EventTarget & HTMLFormElement }) {
-		const data = new FormData(event.currentTarget);
-		console.log(data);
-		const response = await fetch('festival/' + festivalId + '/comments', {
+	function handleSubmit(e: SubmitEvent) {
+		const formData = new FormData(e.target as HTMLFormElement);
+		fetch(festivalId + '/comments', {
 			method: 'POST',
-			body: data
+			body: formData
+		}).then(value => {
+			console.log(value);
 		});
-		/*
-		const result: ActionResult = deserialize(await response.text());
-
-		if (result.type === 'success') {
-			// rerun all `load` functions, following the successful update
-			await invalidateAll();
-		}
-
-		applyAction(result);
-		 */
 	}
 </script>
 
-<form>
-	<label for="comment">Kommentar: </label>
-	<input type="text" id="comment" name="comment" />
+<form on:submit|preventDefault={handleSubmit}>
+	<label for='comment'>Kommentar: </label>
+	<input type='text' id='comment' name='comment' />
 	<p>
-		<button on:submit|preventDefault={handleSubmit}> Absenden </button>
+		<button type='submit'>Absenden</button>
 	</p>
 </form>
--- src/routes/festival/[festival_id]/comments/+server.ts
+++ src/routes/festival/[festival_id]/comments/+server.ts
@@ -1,3 +1,17 @@
-export function POST() {
-	console.log('got input');
+import { RequestEvent } from '@sveltejs/kit';
+import { SessionTokenUser } from '$lib/models/user/SessionTokenUser';
+import { extractUser } from '$lib/services/user-service';
+
+export async function POST(request: RequestEvent): Promise<Response> {
+	const data = await request.request.formData();
+	const comment = data.get('comment');
+	if (comment) {
+		const user: SessionTokenUser | null = extractUser(request.cookies.get('session'));
+		if (user) {
+			console.log('Comment: ', comment);
+			console.log('user: ', user.nickname);
+			return new Response(null, { status: 200 });
+		}
+	}
+	return new Response(null, { status: 500 });
 }
