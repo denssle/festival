6c1c7e44b58ec9a138ffca91d13cac004634d99c
Dominik Hellweg <dominik.hellweg@protonmail.com>
2025-10-17 20:34:55 +0200

    remove host

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
-		host: '0.0.0.0',
 		allowedHosts: ['enzlor.uber.space', 'festival.enzlor.uber.space']
 	}
 });
